454c5817b417fd1d5780520a44e5acfb1b9af156
jtagcat <git-12dbd862@jtag.cat>
2022-10-29 22:15:37 +0300

    Add internal/Parallel()

---
+++ internal/parallel.go
@@ -0,0 +1,44 @@
+package internal
+
+import (
+	"sync"
+
+	"golang.org/x/sync/errgroup"
+)
+
+// TODO: errgroup instant fail?
+// TODO: ordered results?
+// WIP
+//
+// Parallel wraps errgroup, and collects results to an UNORDERED slice.
+// jc: I don't understand ctx enough to return the first error, dismiss results, and kill any running (sub)goroutines.
+//
+// fn usually consists of a for loop of some kind (ex: bufio.Scanner).
+// Inside the for loop, it shall call g.Go(<something>).
+//
+// Don't forget to do i, xyz := xyz, hello before g.Go(func() error { return fooBar(i, xyz, returnc) })
+// (shadowing) https://golang.org/doc/faq#closures_and_goroutines
+func Parallel[T any](fn func(g *errgroup.Group, returnc chan T) error) (output []T, _ error) {
+	g, returnc := new(errgroup.Group), make(chan T)
+
+	fnErr := fn(g, returnc)
+
+	var readOK sync.Mutex
+	readOK.Lock()
+	go func() {
+		defer readOK.Unlock()
+		for r := range returnc {
+			output = append(output, r)
+		}
+	}()
+
+	err := g.Wait()
+	close(returnc)
+	readOK.Lock() // wait for returnc to be fully flushed to output
+
+	if err != nil {
+		return output, err
+	}
+
+	return output, fnErr
+}
---
+++ internal/parallel_test.go
@@ -0,0 +1,47 @@
+package internal_test
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"sort"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/jtagcat/simple/internal"
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/sync/errgroup"
+)
+
+var errBoo = errors.New("I don't like boos")
+
+func exampleNestedFn(line string, linenum int, returnc chan string) error {
+	if line == "boo" {
+		return fmt.Errorf("%d: %w", linenum, errBoo)
+	}
+	if line == "four" {
+		time.Sleep(2 * time.Second) // this is not aborted on error, todo
+	}
+
+	returnc <- "hello " + line
+	return nil
+}
+
+func TestParallel(t *testing.T) {
+	input := strings.NewReader("boo\none\ntwo\nthree\nfour")
+
+	output, err := internal.Parallel(func(g *errgroup.Group, returnc chan string) error {
+		scanner := bufio.NewScanner(input)
+		for i := 1; scanner.Scan(); i++ {
+			line, linenum := scanner.Text(), i
+			g.Go(func() error {
+				return exampleNestedFn(line, linenum, returnc)
+			})
+		}
+		return scanner.Err()
+	})
+	sort.Strings(output)
+	assert.Equal(t, []string{"hello four", "hello one", "hello three", "hello two"}, output)
+	assert.ErrorContains(t, err, errBoo.Error())
+}
