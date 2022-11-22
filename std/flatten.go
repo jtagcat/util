b559e91e099d141f6176ff6e59efd1b99181ac60
jtagcat <git-12dbd862@jtag.cat>
2022-11-23 00:33:37 +0200

    add flatten

---
+++ std/flatten.go
@@ -0,0 +1,64 @@
+package std
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path"
+)
+
+// Flatten a directory recursively
+func Flatten(name string) error {
+	dir, err := os.ReadDir(name)
+	if err != nil {
+		return fmt.Errorf("listing directory: %w", err)
+	}
+
+	for _, item := range dir {
+		if !item.IsDir() {
+			continue
+		}
+
+		err := flattenSubdir(name, path.Join(name, item.Name()))
+		if err != nil {
+			return fmt.Errorf("flattening subdirectory: %w", err)
+		}
+	}
+
+	return nil
+}
+
+func flattenSubdir(flattenTarget, name string) error {
+	dir, err := os.ReadDir(name)
+	if err != nil {
+		return fmt.Errorf("listing directory: %w", err)
+	}
+
+	for _, sub := range dir {
+		if sub.IsDir() {
+			err := flattenSubdir(flattenTarget, path.Join(name, sub.Name()))
+			if err != nil {
+				return err
+			}
+			continue
+		}
+
+		oldPath := path.Join(name, sub.Name())
+		newPath := path.Join(flattenTarget, sub.Name())
+
+		_, err := os.Stat(newPath)
+		if !errors.Is(err, os.ErrNotExist) {
+			return fmt.Errorf("flattening %s: %s: %w", oldPath, newPath, os.ErrExist)
+		}
+
+		err = os.Rename(oldPath, newPath)
+		if err != nil {
+			return fmt.Errorf("moving file: %w", err)
+		}
+	}
+
+	if err := os.Remove(name); err != nil {
+		return fmt.Errorf("removing emptied directory: %w", err)
+	}
+	return nil
+}
---
+++ std/flatten_test.go
@@ -0,0 +1,54 @@
+package std_test
+
+import (
+	"os"
+	"path"
+	"testing"
+
+	"github.com/jtagcat/util/std"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestFlatten(t *testing.T) {
+	dir := t.TempDir()
+	assert.Nil(t, os.MkdirAll(path.Join(dir, "hello", "world"), 0o770))
+
+	_, err := os.Create(path.Join(dir, "hello", "foo"))
+	assert.Nil(t, err)
+	_, err = os.Create(path.Join(dir, "hello", "world", "bar"))
+	assert.Nil(t, err)
+
+	assert.Nil(t, std.Flatten(dir))
+
+	dirS, err := os.ReadDir(dir)
+	assert.Nil(t, err)
+
+	type TestDir struct {
+		Name  string
+		IsDir bool
+	}
+
+	var got []TestDir
+	for _, f := range dirS {
+		got = append(got, TestDir{
+			Name:  f.Name(),
+			IsDir: f.IsDir(),
+		})
+	}
+
+	assert.Equal(t, []TestDir{
+		{Name: "foo"}, {Name: "bar"},
+	}, got)
+}
+
+func TestFlattenConflict(t *testing.T) {
+	dir := t.TempDir()
+	assert.Nil(t, os.MkdirAll(path.Join(dir, "hello", "world"), 0o770))
+
+	_, err := os.Create(path.Join(dir, "hello", "foo"))
+	assert.Nil(t, err)
+	_, err = os.Create(path.Join(dir, "hello", "world", "foo"))
+	assert.Nil(t, err)
+
+	assert.ErrorIs(t, std.Flatten(dir), os.ErrExist)
+}
