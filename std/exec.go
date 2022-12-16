a63804e5eebab2748ea6ce5ccab44612ee2bc2fa
jtagcat <git-12dbd862@jtag.cat>
2022-12-16 13:35:50 +0200

    add std runCmdWithCtx

---
+++ std/exec.go
@@ -0,0 +1,32 @@
+package std
+
+import (
+	"context"
+	"fmt"
+	"os/exec"
+)
+
+func RunCmdWithCtx(ctx context.Context, cmd *exec.Cmd) error {
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	default:
+	}
+
+	if err := cmd.Start(); err != nil {
+		return fmt.Errorf("starting command: %w", err)
+	}
+
+	wait := make(chan error)
+	go func() {
+		wait <- cmd.Wait()
+		close(wait)
+	}()
+
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case err := <-wait:
+		return err
+	}
+}
