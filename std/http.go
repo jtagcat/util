3b941415fd3037e040f4ec9c5e776bbc0f17bf21
jtagcat <git-12dbd862@jtag.cat>
2023-10-04 23:02:46 +0300

    http with ctx

---
+++ std/http.go
@@ -0,0 +1,26 @@
+package std
+
+import (
+	"context"
+	"io"
+	"net/http"
+)
+
+// http.Post() + ctx
+func PostWithContext(ctx context.Context, c *http.Client, url, contentType string, body io.Reader) (resp *http.Response, err error) {
+	req, err := http.NewRequestWithContext(ctx, "POST", url, body)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", contentType)
+	return c.Do(req)
+}
+
+// http.Get()+ ctx
+func GetWithContext(ctx context.Context, c *http.Client, url string) (resp *http.Response, err error) {
+	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+	return c.Do(req)
+}
