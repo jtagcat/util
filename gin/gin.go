3b34773ccc19cc3a1e41c8d0c3a2118a01d517c4
jtagcat <git-12dbd862@jtag.cat>
2023-10-06 00:08:00 +0300

    Change HandlerWithErr format

--- gin/gin.go
+++ gin/gin.go
@@ -16,31 +16,40 @@ func Cookie(c *gin.Context, name string) string {
 // Do not change this outside of init()
 var ErrorPage = "error.html"
 
-func HandlerWithErr(f func(*gin.Context) (status int, err string)) func(*gin.Context) {
-	return func(c *gin.Context) {
-		code, err := f(c)
+func HandlerWithErr(f func(*gin.Context, *Context) (status int, err string)) func(*gin.Context) {
+	return func(ctx *gin.Context) {
+		code, err := f(ctx, &Context{ctx})
 		if err != "" {
-			c.HTML(code, ErrorPage, gin.H{
+			ctx.HTML(code, ErrorPage, gin.H{
 				"err": fmt.Sprintf("%d %s: %s", code, http.StatusText(code), err),
 			})
-			c.Abort()
+			ctx.Abort()
 			return
 		}
 
 		if code != 0 {
-			c.Status(code)
+			ctx.Status(code)
 		}
 	}
 }
 
+type Context struct {
+	ctx *gin.Context
+}
+
 // Wrapper for HandlerWithErr format
-func HTML(c *gin.Context, status int, name string, obj any) (int, string) {
-	c.HTML(status, name, obj)
+func (w *Context) HTML(status int, name string, obj any) (int, string) {
+	w.ctx.HTML(status, name, obj)
 	return 0, ""
 }
 
 // Wrapper HandlerWithErr format
-func Redirect(c *gin.Context, status int, location string) (int, string) {
-	c.Redirect(status, location)
+func (w *Context) Redirect(status int, location string) (int, string) {
+	w.ctx.Redirect(status, location)
 	return 0, ""
+}
+
+// Wrapper HandlerWithErr format
+func (w *Context) Cookie(name string) string {
+	return Cookie(w.ctx, name)
 }
