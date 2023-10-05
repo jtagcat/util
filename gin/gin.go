016c36c3a8e35d99870682572290115b6878eaf9
jtagcat <git-12dbd862@jtag.cat>
2023-10-05 23:52:30 +0300

    gin HandlerWithErr

--- gin/gin.go
+++ gin/gin.go
@@ -1,9 +1,32 @@
-package std
+package gin
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 // c.Cookie(), without ok var (instead, check if empty)
 func Cookie(c *gin.Context, name string) string {
 	njom, _ := c.Cookie(name)
 	return njom
+}
+
+// Uses error.html provided by consumer
+func HandlerWithErr(f func(*gin.Context) (status int, err string)) func(*gin.Context) {
+	return func(c *gin.Context) {
+		code, err := f(c)
+		if err != "" {
+			c.HTML(code, "error.html", gin.H{
+				"err": fmt.Sprintf("%d %s: %s", code, http.StatusText(code), err),
+			})
+			c.Abort()
+			return
+		}
+
+		if code != 0 {
+			c.Status(code)
+		}
+	}
 }
